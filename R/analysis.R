44b35e3b2cf349274e71f5d9256948f3d0ed5a3d
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-08-13 14:13:21 -0500

    correspondence analysis added

--- R/analysis.R
+++ R/analysis.R
@@ -2,3 +2,22 @@
 library(readxl)
 tabla_datos <- read_xlsx("datos/..")
 View(tabla_datos)
+
+## Análisis de Correspondencias
+library(ca)
+acm <- ca(tabla_datos)
+summary(acs)
+plot(summary(acs)$scree, type="l") #Gráfico de codo (tal vez sea así)
+
+#Graficar
+plot(acs)
+plot(acs, lines = TRUE) #Se observa la dependencia
+plot(acs, arrows = TRUE)
+plot(acs, arrows = c(TRUE, FALSE))
+plot(acs, arrows = c(FALSE, TRUE))
+plot(acs, arrows = c(TRUE, TRUE))
+plot(acs, arrows = c(FALSE, TRUE), lines = TRUE)
+
+acm <- mjca(tabla_datos,lambda = "indicator",nd=NA)
+plot(acm)
+biplot(acm$rowpcoord,acm$colpcoord)
