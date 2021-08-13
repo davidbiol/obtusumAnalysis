18b3df9f6ec0bb482615cd7e9bbaa5ef0ec83bd5
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-08-13 14:19:44 -0500

    component analysis added

--- R/analysis.R
+++ R/analysis.R
@@ -21,3 +21,33 @@ plot(acs, arrows = c(FALSE, TRUE), lines = TRUE)
 acm <- mjca(tabla_datos,lambda = "indicator",nd=NA)
 plot(acm)
 biplot(acm$rowpcoord,acm$colpcoord)
+#------------------------------------------------------------------------------
+
+## Analisis de Componentes
+acp <- prcomp(tabla_datos)
+plot(ACP, type="l") #Grafico de sedimentacion o de codo
+plot(ACP$rotation)
+biplot(ACP)
+plot(ACP$x[,1], ACP$x[,2])
+#PCA1 vs PCA2, diferenciando por especie
+plot(ACP$x[,1], ACP$x[,2], col = ifelse(crabs$sp=="O", "orange", "blue"), pch=16, xlab="PCA,1", ylab="PCA,2")
+library(scatterplot3d)
+scatterplot3d(ACP$x[,1], ACP$x[,2], ACP$x[,3], color=ifelse(crabs$sp=="O", "orange", "blue"))
+
+#Graficar ggfortify
+library(ggfortify)
+autoplot(ACP)
+autoplot(ACP, colour = 'sex')
+autoplot(ACP, data = crabs, colour = 'sex', label=TRUE,label.size=3)
+autoplot(ACP, data = crabs, colour = 'sex', shape=FALSE,label.size=3)
+autoplot(ACP, data = crabs, colour = 'sex', shape=FALSE,label.size=3, loadings=TRUE)
+autoplot(ACP, data = crabs, colour = 'sex', shape=FALSE,label.size=3,
+         loadings = TRUE, loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 3,
+         loadings.label.colour='blue')
+#Gráfico final con todo
+autoplot(ACP, data = crabs, colour = ifelse(crabs$sex=="M"&crabs$sp=="O", "purple", ifelse(crabs$sex=="M"&crabs$sp=="B", "green", ifelse(crabs$sex=="F"&crabs$sp=="O", "orange", ifelse(crabs$sex=="F"&crabs$sp=="B", "blue", "black")))),
+         shape=FALSE,label.size=3,
+         loadings = TRUE, loadings.colour = 'red',
+         loadings.label = TRUE, loadings.label.size = 3,
+         loadings.label.colour='red')
